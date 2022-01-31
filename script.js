10b911f74f2f0f72005f56d6aa871faf1adfe07f
xavifumi <xavifumi@gmail.com>
2022-01-31 11:49:54 +0100

    v3
    light / dark modes

--- script.js
+++ script.js
@@ -176,3 +176,18 @@ function neteja(){
     localStorage.setItem("taskList",JSON.stringify(taskListFiltrada));
     dibuixaTasques();
 }
+
+
+/*---------------- */
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
